3df68fcd5a041c5ea01b3ddfd76df8cdfd903060
Juhi Kumari <juhikumari98825@gmail.com>
2025-05-19 20:11:54 +0530

    add numbers programm

---
+++ practicePrograms/AddNums.java
@@ -0,0 +1,10 @@
+package practicePrograms;
+
+public class AddNums {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 7;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
